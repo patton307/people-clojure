404293ad8ecf2063ea0bbcc0b8d45623b4304f1c
landonkail <landon.kail@gmail.com>
2015-11-24 11:39:26 -0500

    parsed .csv file and converted it into a .edn file

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,7 +1,25 @@
 (ns people-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [people (slurp "people.csv")
+        people (str/split-lines people)
+        people (map (fn [line]
+                       (str/split line #","))
+                      people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line]
+                      (interleave header line))
+                    people)
+        people (map (fn [line]
+                      (apply hash-map line))
+                    people)
+        people (walk/keywordize-keys people)
+        people (filter (fn [line]
+                         (= "Brazil" (:country line)))
+                       people)]
+    (spit "filtered_people.edn"
+          (pr-str people))))
